58de347610ed13e72993de7d3ee034ba27cccc2c
Robert Ing <rmi22186@gmail.com>
2014-11-11 18:06:20 -0800

    able to pass string and numbers, but not arrays or objects yet

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,26 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+
+  if (typeof obj === 'string') {
+      return ('"' + obj + '"');
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  if (Array.isArray(obj)) {
+    return ('"[' + obj + ']"');
+  } 
+
+
+  //objectKeys has all of the keys from obj in an array
+  var objectKeys = Object.keys(obj);
+
+		var i = 0;
+		var objectKeyLength = objectKeys.length;
+		while (i < objectKeyLength) {
+	  	return '{"'+ objectKeys[i] + ": " + stringifyJSON(obj[objectKeys[i]]) + '}';
+	  	i++
+		}
 };
